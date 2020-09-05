ee39ea77e58cb81b7d5e5434ea6086eada3a8b1c
Igarugueri <igaru74@gmail.com>
2020-09-05 20:15:34 +0200

    dotestint stage

--- Testing-R.R
+++ Testing-R.R
@@ -1,3 +1,6 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+print("I want to carry out a new stage")
+print("...let me see what happend....")
+print("...???...")
